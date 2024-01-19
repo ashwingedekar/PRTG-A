6cd66b4a860ff3d376120f8bf579ef1d76bfa5a0
Ashwin Gedekar <116587163+ashwingedekar@users.noreply.github.com>
2024-01-19 15:47:27 +0530

    new file added

--- Conecteivity101.py
+++ Conecteivity101.py
@@ -1,4 +1,5 @@
 import requests
-response = requests.get("https://tp-prtg-101-100.comtelindia.com:10443/api/table.xml?content=sensors&columns=sensor&apitoken=TF2M3TXUJJEM3WCDC5YMMXLRDCXQTQAAE56W3L4MQM======")
+response = requests.get("https://tp-prtg-101-100.comtelindia.com:10443/api/table.xml?content=sensors&columns=sensor&username=Ashwin.Gedekar&passhash=3422185132")
 print(response)
 #TF2M3TXUJJEM3WCDC5YMMXLRDCXQTQAAE56W3L4MQM======
+#api_endpoint = ''
--- Conecteivity_localhost.py
+++ Conecteivity_localhost.py
@@ -1,3 +1,4 @@
 import requests
-response = requests.get("https://jp-vivekp-40-58.comtelindia.com/api/table.xml?content=sensors&columns=sensor&apitoken=4NMXTANEV3HEFWYGISUQRIHS725KOOQWZ5EWWLCELE======")
+response = requests.get("https://me-ncm-01.comtelindia.com:8061/api/json/v2/ncmdevice/listAllDevices?apiKey=004869eea1f43365a547efb9c410f75e")
+#http://localhost:8060/api/json/v2/ncmdevice/listAllDevices?apiKey=004869eea1f43365a547efb9c410f75e
 print(response)
